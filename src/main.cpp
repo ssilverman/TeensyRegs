b49336dab610b05408a94e0cfa493a721db6dd44
Shawn Silverman <shawn@pobox.com>
2026-07-17 07:11:04 -0700

    main: Add a way to toggle between the old and new ways
    Set USE_OLD_WAY to 1 or 0.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+// To use the old way, set this to 1.
+#define USE_OLD_WAY 0
+
 #include <cinttypes>
 #include <cstdint>
 #include <cstdio>
@@ -5,14 +8,18 @@
 #include <Arduino.h>
 #include <elapsedMillis.h>
 
+#if !USE_OLD_WAY
 #include "hardware/teensy4/CCM.h"
 #include "hardware/teensy4/CCM_ANALOG.h"
 #include "hardware/teensy4/DCB.h"
 #include "hardware/teensy4/DWT.h"
 #include "hardware/teensy4/IOMUXC_GPR.h"
 #include "hardware/teensy4/SCB.h"
+#endif  // !USE_OLD_WAY
 
+#if !USE_OLD_WAY
 using namespace qindesign::hardware::teensy4;
+#endif  // !USE_OLD_WAY
 
 static void reboot();
 static void enable_enet_clocks();
@@ -26,6 +33,17 @@ static constexpr float kRunTime      = 20.0f;
 static bool hasARMCounter = false;
 static elapsedMillis mainTimer;
 static elapsedMillis counterTimer;
+
+#if USE_OLD_WAY
+// #define CLRSET(reg, clear, set) ((reg) = ((reg) & ~(clear)) | (set))
+
+// Clears some bits in a 32-bit register and then sets others.
+[[gnu::always_inline]]
+static inline void clearAndSet32(volatile uint32_t* const reg,
+                                 const uint32_t clear, const uint32_t set) {
+  *reg = (*reg & ~clear) | set;
+}
+#endif  // USE_OLD_WAY
 
 // Main program setup.
 void setup() {
@@ -72,13 +90,17 @@ void loop() {
 
 // Reboots the Teensy.
 static void reboot() {
+#if !USE_OLD_WAY
   SCB->AIRCR = SCB_AIRCR_VECTKEY(0x05fa) | SCB_AIRCR_SYSRESETREQ(1);
+#else
+  SCB_AIRCR = 0x05fa0000 | 0x0004;
+#endif  // !USE_OLD_WAY
 }
 
 // Enables the Ethernet-related clocks. See also disable_enet_clocks().
 static void enable_enet_clocks() {
+#if !USE_OLD_WAY
   // Enable the Ethernet clock
-
   CCM_CCGR1_ENET = kCCM_CCGR_ON;
 
   // Configure PLL6 for 50 MHz (page 1112)
@@ -105,10 +127,42 @@ static void enable_enet_clocks() {
   IOMUXC_GPR_GPR1_ENET1_CLK_SEL     = 0;
   IOMUXC_GPR_GPR1_ENET_IPG_CLK_S_EN = 1;
   IOMUXC_GPR_GPR1_ENET1_TX_CLK_DIR  = 1;
+#else
+  // Enable the Ethernet clock
+  CCM_CCGR1 |= CCM_CCGR1_ENET(CCM_CCGR_ON);
+
+  // Configure PLL6 for 50 MHz (page 1112)
+  CCM_ANALOG_PLL_ENET_SET = CCM_ANALOG_PLL_ENET_BYPASS;
+  CCM_ANALOG_PLL_ENET_CLR = 0
+                            | CCM_ANALOG_PLL_ENET_BYPASS_CLK_SRC(3)
+                            | CCM_ANALOG_PLL_ENET_ENET2_DIV_SELECT(3)
+                            | CCM_ANALOG_PLL_ENET_DIV_SELECT(3)
+                            ;
+  CCM_ANALOG_PLL_ENET_SET = 0
+                            | CCM_ANALOG_PLL_ENET_ENET_25M_REF_EN
+                            // | CCM_ANALOG_PLL_ENET_ENET2_REF_EN
+                            | CCM_ANALOG_PLL_ENET_ENABLE
+                            // | CCM_ANALOG_PLL_ENET_ENET2_DIV_SELECT(1)
+                            | CCM_ANALOG_PLL_ENET_DIV_SELECT(1)
+                            ;
+  CCM_ANALOG_PLL_ENET_CLR = CCM_ANALOG_PLL_ENET_POWERDOWN;
+  while ((CCM_ANALOG_PLL_ENET & CCM_ANALOG_PLL_ENET_LOCK) == 0) {
+    // Wait for PLL lock
+  }
+  CCM_ANALOG_PLL_ENET_CLR = CCM_ANALOG_PLL_ENET_BYPASS;
+  // printf("PLL6 = %08" PRIX32 "h (should be 80202001h)\n", CCM_ANALOG_PLL_ENET);
+
+  // Configure REFCLK to be driven as output by PLL6 (page 325)
+  clearAndSet32(
+      &IOMUXC_GPR_GPR1,
+      IOMUXC_GPR_GPR1_ENET1_CLK_SEL,
+      IOMUXC_GPR_GPR1_ENET_IPG_CLK_S_EN | IOMUXC_GPR_GPR1_ENET1_TX_CLK_DIR);
+#endif  // !USE_OLD_WAY
 }
 
 // Disables everything enabled with enable_enet_clocks().
 static void disable_enet_clocks() {
+#if !USE_OLD_WAY
   // Configure REFCLK
   IOMUXC_GPR_GPR1_ENET1_TX_CLK_DIR = 0;
 
@@ -122,11 +176,41 @@ static void disable_enet_clocks() {
 
   // Disable the clock for ENET
   CCM_CCGR1_ENET = kCCM_CCGR_OFF;
+#else
+  // Configure REFCLK
+  clearAndSet32(&IOMUXC_GPR_GPR1, IOMUXC_GPR_GPR1_ENET1_TX_CLK_DIR, 0);
+
+  // Stop the PLL (first bypassing)
+  CCM_ANALOG_PLL_ENET_SET = CCM_ANALOG_PLL_ENET_BYPASS;
+  CCM_ANALOG_PLL_ENET = 0
+                        | CCM_ANALOG_PLL_ENET_BYPASS         // Reset to default
+                        | CCM_ANALOG_PLL_ENET_POWERDOWN
+                        | CCM_ANALOG_PLL_ENET_DIV_SELECT(1)
+                        ;
+
+  // Disable the clock for ENET
+  CCM_CCGR1 &= ~CCM_CCGR1_ENET(CCM_CCGR_ON);
+#endif  // !USE_OLD_WAY
 }
+
+#if USE_OLD_WAY
+// DWT Control Register
+static const auto kDWT_CTRL   = reinterpret_cast<volatile uint32_t*>(0xE0001000);
+// DWT Cycle Count Register
+static const auto kDWT_CYCCNT = reinterpret_cast<volatile uint32_t*>(0xE0001004);
+// Debug Exeption and Monitor Control Register
+static const auto kDEMCR      = reinterpret_cast<volatile uint32_t*>(0xE000EDFC);
+
+// Values
+static constexpr auto kDEMCR_TRCENA       = uint32_t{1 << 24};
+static constexpr auto kDWT_CTRL_CYCCNTENA = uint32_t{1 <<  0};
+static constexpr auto kDWT_CTRL_NOCYCCNT  = uint32_t{1 << 25};
+#endif  // USE_OLD_WAY
 
 // Initializes the cycle counter and returns whether it's supported. This uses
 // heuristics and isn't guaranteed to work for all cases.
 static bool arm_high_resolution_clock_init() {
+#if !USE_OLD_WAY
   // First enable DWT and check
   if (DCB_DEMCR_TRCENA == 0) {
     DCB_DEMCR_TRCENA = 1;
@@ -151,10 +235,40 @@ static bool arm_high_resolution_clock_init() {
       return false;
     }
   }
+#else
+  // First enable DWT and check
+  if ((*kDEMCR & kDEMCR_TRCENA) == 0) {
+    *kDEMCR |= kDEMCR_TRCENA;
+
+    // Check that it was enabled
+    if ((*kDEMCR & kDEMCR_TRCENA) == 0) {
+      return false;
+    }
+  }
+
+  // Next, check the obvious feature presence
+  if ((*kDWT_CTRL & kDWT_CTRL_NOCYCCNT) != 0) {
+    return false;
+  }
+
+  // Next, check if the cycle count is enabled
+  if ((*kDWT_CTRL & kDWT_CTRL_CYCCNTENA) == 0) {
+    *kDWT_CTRL |= kDWT_CTRL_CYCCNTENA;
+
+    // Check that it was enabled
+    if ((*kDWT_CTRL & kDWT_CTRL_CYCCNTENA) == 0) {
+      return false;
+    }
+  }
+#endif  // !USE_OLD_WAY
 
   return true;
 }
 
 static uint32_t arm_high_resolution_clock_count() {
+#if !USE_OLD_WAY
   return DWT->CYCCNT;
+#else
+  return ARM_DWT_CYCCNT;
+#endif  // !USE_OLD_WAY
 }
